c834903e6e7207b533c1432dab1f4b02f63fe754
Duy Do <doquocduy@gmail.com>
2014-10-19 15:12:21 +0700

    add @Assisted

--- src/main/java/org/elasticsearch/index/analysis/VietnameseAnalyzerProvider.java
+++ src/main/java/org/elasticsearch/index/analysis/VietnameseAnalyzerProvider.java
@@ -1,11 +1,12 @@
 package org.elasticsearch.index.analysis;
 
+import org.apache.lucene.analysis.vi.VietnameseAnalyzer;
 import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.inject.assistedinject.Assisted;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.Index;
 import org.elasticsearch.index.settings.IndexSettings;
-import org.apache.lucene.analysis.vi.VietnameseAnalyzer;
 
 /**
  * @author duydo
@@ -14,11 +15,12 @@ public class VietnameseAnalyzerProvider extends AbstractIndexAnalyzerProvider<Vi
     private final VietnameseAnalyzer analyzer;
 
     @Inject
-    public VietnameseAnalyzerProvider(Index index, @IndexSettings Settings indexSettings,  Environment env, String name,
-                                      Settings settings) {
+    public VietnameseAnalyzerProvider(Index index, @IndexSettings Settings indexSettings, Environment env,
+                                      @Assisted String name,
+                                      @Assisted Settings settings) {
         super(index, indexSettings, name, settings);
         analyzer = new VietnameseAnalyzer(version,
-                Analysis.parseStopWords(env, settings, VietnameseAnalyzer.getDefaultStopSet(), version, true)
+                Analysis.parseStopWords(env, settings, VietnameseAnalyzer.getDefaultStopSet(), version)
         );
     }
 
