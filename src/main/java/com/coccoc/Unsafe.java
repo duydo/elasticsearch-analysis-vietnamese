ae71f317b93663f80a3b6fd643bf890000b5bad4
Tyler Nguyen <tyler.nguyen1191@gmail.com>
2026-03-14 02:33:04 +0700

    feat: add unit and integration tests

--- src/main/java/com/coccoc/Unsafe.java
+++ src/main/java/com/coccoc/Unsafe.java
@@ -1,28 +1,23 @@
 package com.coccoc;
 
-import java.lang.reflect.Field;
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
 
-@SuppressWarnings("removal")
+/**
+ * Memory access utilities using the Java 21 Foreign Function & Memory (FFM) API
+ * as a replacement for the terminally deprecated sun.misc.Unsafe address-based methods.
+ */
 public class Unsafe {
 
-  public static final sun.misc.Unsafe UNSAFE;
-
-  static {
-    try {
-      Field field = sun.misc.Unsafe.class.getDeclaredField("theUnsafe");
-      field.setAccessible(true);
-      UNSAFE = (sun.misc.Unsafe) field.get(null);
-    } catch (Exception e) {
-      throw new ExceptionInInitializerError(e);
-    }
-  }
-
-
   public static int getInt(long address) {
-    return UNSAFE.getInt(address);
+    return MemorySegment.ofAddress(address)
+        .reinterpret(Integer.BYTES)
+        .get(ValueLayout.JAVA_INT_UNALIGNED, 0);
   }
 
   public static long getLong(long address) {
-    return UNSAFE.getLong(address);
+    return MemorySegment.ofAddress(address)
+        .reinterpret(Long.BYTES)
+        .get(ValueLayout.JAVA_LONG_UNALIGNED, 0);
   }
 }
